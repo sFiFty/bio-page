33c727ee3f2066019c45ce8a027b698663fb1f17
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-04-06 08:55:55 +0300

    understood services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
-
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -35,21 +35,24 @@ export class HomeComponent implements OnInit {
 
   goals = [];
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   addItem() {
     this.goals.push(this.goalTxt);
     this.goalTxt = '';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   removeItem(i) {
     console.log(i);
-    this.goals.splice(i, 1);
+    this._data.changeGoal(this.goals);
   }
 
 }
