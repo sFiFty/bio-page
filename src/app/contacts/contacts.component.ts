d792c7dd7ad51a1642e7fb0f0d7337acee1708f5
sFiFty <alexandr.rudin.my@gmail.com>
2018-05-12 18:57:25 +0300

    disabled styles incapsulation

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+  styleUrls: ['./contacts.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ContactsComponent implements OnInit {
 
