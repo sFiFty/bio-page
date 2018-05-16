c3bce897b94f8df5fd116fb1fc59719fd4125e60
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-05-16 08:07:27 +0300

    hardcoded year if NaN

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,6 +27,6 @@ export class HomeComponent implements OnInit {
     let birthdayTime = getTime(birthday);
     let ageDifMs = currentTime - birthdayTime;
     let ageDate = format(ageDifMs);
-    return Math.abs(getYear(format(ageDate)) - 1970);
+    return Math.abs(getYear(format(ageDate)) - 1970) || 28;
   }
 }
