c58d5277fa47a75a84270d00bfb04f02f8cea95b
sFiFty <alexandr.rudin.my@gmail.com>
2018-06-24 16:55:23 +0300

    removed expand logic from controller

--- src/app/companies/companies.component.ts
+++ src/app/companies/companies.component.ts
@@ -8,17 +8,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class CompaniesComponent implements OnInit {
 
-  companiesExpanded: object = {
-    '3shape': false,
-    'decorist': false,
-    'codemotion': false
-  };
-
   constructor() { }
-
-  onExpand = company => {
-    this.companiesExpanded[company] = !this.companiesExpanded[company];
-  }
 
   ngOnInit() {
   }
